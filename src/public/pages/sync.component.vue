d95b4a384c29ad599dcf3495a4ca0cbb30dd5289
sergioJM05 <sergiogino8@hotmail.com>
2025-06-21 13:05:48 -0500

    feat(monitoring): add connection guide view.

--- src/public/pages/sync.component.vue
+++ src/public/pages/sync.component.vue
@@ -1,12 +1,16 @@
 <script>
 
+import AvailableDevicesManagement
+  from "../../assets-and-resources-management/pages/available-devices-management.component.vue";
+
 export default {
-  name: "sync"
+  name: "sync",
+  components: {AvailableDevicesManagement}
 }
 </script>
 
 <template>
-
+<available-devices-management></available-devices-management>
 </template>
 
 <style scoped>
