8f8b82b560047d764e4aa97ef20cc8bf19748893
sergioJM05 <sergiogino8@hotmail.com>
2025-06-24 09:20:55 -0500

    feat(monitoring): add vehicle-failure assembler, entity and service.

--- src/monitoring/pages/failures-management.component.vue
+++ src/monitoring/pages/failures-management.component.vue
@@ -1,6 +1,10 @@
 <script>
 import Analysis from "../components/analysis.component.vue";
 import Scan from "../components/scan.component.vue";
+import {VehicleSessionService} from "../../shared/services/vehicle-session.service.js";
+import {VehicleService} from "../../assets-and-resources-management/services/vehicle.service.js";
+import {BadPracticeService} from "../services/bad-practice.service.js";
+import {OdbErrorService} from "../services/odb-error.service.js";
 
 export default {
   name: "failures-management",
@@ -12,9 +16,21 @@ export default {
       vehicleService: null,
       badPracticeService: null,
       odbErrorService: null,
-      failureService: null
+      failureService: null,
+
+      vehicleFailures: null,
     };
   },
+  created(){
+    this.vehicleId=VehicleSessionService.getVehicleId();
+    if(!this.vehicleId) this.$router.push("/login");
+
+    this.vehicleService = new VehicleService();
+    this.badPracticeService = new BadPracticeService();
+    this.odbErrorService = new OdbErrorService();
+
+
+  }
 
 }
 </script>
