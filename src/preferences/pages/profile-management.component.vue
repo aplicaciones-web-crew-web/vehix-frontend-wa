40cd2ebbb362a52156e4d3e6c01302b681f4c968
Piero Elescano <pieroelescano300@gmail.com>
2025-07-04 00:43:54 -0500

    feat: add profile

---
+++ src/preferences/pages/profile-management.component.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "profile-management.component"
+})
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
